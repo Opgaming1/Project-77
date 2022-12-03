526b948cf2aa0621817a76662c2b0d97a8555c3a
Opgaming1 <91045339+Opgaming1@users.noreply.github.com>
2022-12-03 14:48:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("My first console project");
+
+
+var first_name="Rudransh";
+console.log(first_name);
+
+  var last_name="Salathia";
+  console.log(last_name);
+
+  var full_name=first_name.concat(last_name);
+  console.log(full_name);
+
+  var a=30;
+  var b=35;
+  var add=a+b;
+  console.log(add);
+
+  var sub=a-b;
+  console.log(sub);
+
+  var multi=a*b;
+  console.log(multi);
+
+  var div=a/b;
+  console.log(div);
